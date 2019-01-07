b3597a708bc081aaaec7a9dce724979c83e16c21
Jesse <jessepatch9@gmail.com>
2019-01-07 01:41:29 -0600

    started working on playlist

--- src/app/sidebar/sidebar.service.ts
+++ src/app/sidebar/sidebar.service.ts
@@ -27,6 +27,8 @@ export class SidebarService {
 
   public loadAudioPlayer(url:string) {
     this.audio.src = url;
+    this.audio.volume = 0.5;
+    //this.audio.addEventListener('ended', this.playNextSong());
   }
 
   public getDurationHour():number {
@@ -38,7 +40,7 @@ export class SidebarService {
     while(x > 0) {
       x = x - 3600;
     }
-    return Math.floor(x + 3600);
+    return Math.floor((x + 3600) / 60).toString().padStart(2, '0');
   }
 
   public getDurationSecond() {
@@ -46,26 +48,34 @@ export class SidebarService {
     while(y > 0) {
       y = y - 60;
     }
-    return Math.ceil(y + 60);
+    return Math.ceil(y + 60).toString().padStart(2, '0');
   }
 
   public getCurrentTimeHour():number {
     return Math.floor(this.audio.currentTime / 3600);
   }
 
-  public getCurrentTimeMinute():number {
+  public getCurrentTimeMinute() {
     let x = this.audio.currentTime;
     while(x > 0) {
       x = x - 3600;
     }
-    return Math.floor(x + 3600);
+    return Math.floor((x + 3600) / 60).toString().padStart(2, '0');
   }
 
-  public getCurrentTimeSecond():number {
+  public getCurrentTimeSecond() {
     let y = this.audio.currentTime;
     while(y > 0) {
       y = y - 60;
     }
-    return Math.ceil(y + 60);
+    return Math.ceil(y + 60).toString().padStart(2, '0');
   }
+
+  public addToTrackList() {
+    this.audio.audioTracks[0];
+  }
+
+  // public playNextSong() {
+  //   this.audio.src = 
+  // }
 }
